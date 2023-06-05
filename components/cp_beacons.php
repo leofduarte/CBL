40d2235d5f0b6e0d802c1fbd73f89f845b00bcc2
leofduarte <leandroduarte@ua.pt>
2023-06-05 22:00:55 +0100

    Fix: Filtro Beacons

--- components/cp_beacons.php
+++ components/cp_beacons.php
@@ -32,6 +32,7 @@ require_once("connections/connection.php");
       }
    ?>
 
+
       <div class="toast-container position-absolute p-3" style="z-index: 99; right: 0; top: 4rem;">
          <div id="liveToast" class="toast <?= $color ?>" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="5000">
             <div class="toast-body">
@@ -49,34 +50,81 @@ require_once("connections/connection.php");
       </script>
 
    <?php } ?>
-   <h1 style="color:#393166;" class="mt-5 ms-3">Beacons</h1>
-   <div style="border-radius: 2rem; background-color:#F4F2FF;" class="p-4 mb-5 mt-4">
-      <h2 style="color:#393166" class="mb-4 ">Lista de Beacons</h2>
 
-      <?php
-      // Create a new DB connection
-      $link = new_db_connection();
+   <?php
+   // Create a new DB connection
+   $link = new_db_connection();
 
-      // Create a prepared statement
-      $stmt = mysqli_stmt_init($link);
+   // Create a prepared statement
+   $stmt = mysqli_stmt_init($link);
 
-      // Define the query
-      $query = "SELECT id_beacon, nome_beacon, uuid_beacon, id_obra, nome_obra
+   // Define the query
+   $query = "SELECT id_beacon, nome_beacon, uuid_beacon, id_obra, nome_obra
                 FROM obra
                 right JOIN beacons
-                ON ref_beacons = id_beacon
-                ";
-      //! LIMIT 6 OFFSET 0
+                ON ref_beacons = id_beacon";
+   //! LIMIT 6 OFFSET 0
 
 
-      // Prepare the statement
-      if (mysqli_stmt_prepare($stmt, $query)) {
+   //* FILTRAR COMENTÁRIOS
+   $comObra = null;
+   $semObra = null;
+   if (isset($_GET['filterBy'])) {
+      if ($_GET['filterBy'] == 'SemObra') {
+         // if ($id_obra == NULL) {
+         //    $semObra = "btn-secondary text-white";
+         //    $query = $query . " AND id_obra IS NULL";
+         // } else if ($_GET['filterBy'] == 'ComObra') {
+         //    if ($id_obra != NULL)
+         //       $comObra = "btn-secondary text-white";
+         $query = $query . " WHERE id_obra IS NULL";
+         $semObra = "btn-secondary text-white";
+         // }
+      } else if ($_GET['filterBy'] == 'ComObra') {
+         $query = $query . " WHERE id_obra IS NOT NULL";
+         $comObra = "btn-secondary text-white";
+      }
+   }
 
-         // Execute the prepared statement
-         if (mysqli_stmt_execute($stmt)) {
-            // Bind result variables
-            mysqli_stmt_bind_result($stmt, $id_beacon, $nome_beacon, $uuid_beacon, $id_obra, $nome_obra);
-      ?>
+   // Prepare the statement
+   if (mysqli_stmt_prepare($stmt, $query)) {
+
+      // Execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+         // Bind result variables
+         mysqli_stmt_bind_result($stmt, $id_beacon, $nome_beacon, $uuid_beacon, $id_obra, $nome_obra);
+   ?>
+
+         <h1 style="color:#393166;" class="mt-5 ms-3">Beacons</h1>
+         <div style="border-radius: 2rem; background-color:#F4F2FF;" class="p-4 mb-5 mt-4">
+            <div class="d-flex justify-content-between">
+               <h2 style="color:#393166  justify-content-start" class="mb-4 ">Lista de Beacons</h2>
+
+               <div class="dropdown d-flex align-content-middle me-4" style="margin: 0 0 1rem 0; right: 0;">
+                  <button class=" btn text-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                     <span>Filtrar Por </span></button>
+                  <ul class="dropdown-menu w-100 p-0" aria-labelledby="dropdownMenuButton">
+                     <li>
+                        <a href="./beacons.php" class="btn text-dark w-100" type="button" id="todos">
+                           <span>Todos</span>
+                        </a>
+                     </li>
+                     <hr class="p-0 m-0">
+                     <li>
+                        <a href="./beacons.php?filterBy=SemObra" class="btn w-100 <?= $semObra ?>" type="button" id="SemObra">
+                           <span>Sem obra Associada</span>
+                        </a>
+                     </li>
+                     <hr class="p-0 m-0">
+                     <li>
+                        <a href="./beacons.php?filterBy=ComObra" class="btn w-100 <?= $comObra ?>" type="button" id="inativo">
+                           <span>Com Obra Associada</span>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+
             <table class="data_users mb-5 w-100 text-center">
                <thead class="text-info" style="border-bottom: 1px solid grey">
                   <tr>
@@ -118,22 +166,23 @@ require_once("connections/connection.php");
                   <a class="text-roxo" href=""> <span><i class="bi bi-arrow-right"></i></span></a>
                </div>
             </div>
-   </div>
+         </div>
 
-<?php
 
-         } else {
-            // Execute error
-            echo "Error: " . mysqli_stmt_error($stmt);
-         }
+   <?php
+
       } else {
-         // Errors related with the query
-         echo "Error: " . mysqli_error($link);
+         // Execute error
+         echo "Error: " . mysqli_stmt_error($stmt);
       }
-      // Close statement
-      mysqli_stmt_close($stmt);
+   } else {
+      // Errors related with the query
+      echo "Error: " . mysqli_error($link);
+   }
+   // Close statement
+   mysqli_stmt_close($stmt);
 
-      // Close connection
-      mysqli_close($link);
+   // Close connection
+   mysqli_close($link);
 
-?>
+   ?>
